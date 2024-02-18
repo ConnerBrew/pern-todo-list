4463751a52dbd9900990d6b60853fe9353f81b03
ConnerBrew <cmbrew1306@gmail.com>
2024-02-18 09:52:57 -0500

    updating config for db

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: PWORD,
+    password: "1qaz@WSX3edc$RFV",
     host: "localhost",
     port: 5432,
     database: "perntodo"
